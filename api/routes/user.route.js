5e4ffc7da42e12a70f665655e4bf4a9fbeea26c1
AukTo1380 <auktoafg@gmail.com>
2025-01-14 12:06:29 +0430

    testing the git

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,15 +1,18 @@
-import express from 'express'
-import { test,deleteUser ,signout, updateUsers, geteUsers} from '../controlers/user.controllers.js';
-import { verifyToken } from '../utils/verifyUser.js';
+import express from "express";
+import {
+  test,
+  deleteUser,
+  signout,
+  updateUsers,
+  geteUsers,
+} from "../controlers/user.controllers.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
-const router=express.Router();
+const router = express.Router();
 
-
-router.put('/update/:id', updateUsers,verifyToken);
-router.delete('/delete/:userId',verifyToken,deleteUser)
-router.post('/signout',signout)
-router.get('/getusers',verifyToken,geteUsers)
-
-
+router.put("/update/:id", updateUsers, verifyToken);
+router.delete("/delete/:userId", verifyToken, deleteUser);
+router.post("/signout", signout);
+router.get("/getusers", verifyToken, geteUsers);
 
 export default router;
