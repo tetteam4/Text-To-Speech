4b6fac6ce3062139aba84f06a15e7f37c4122164
AukTo1380 <auktoafg@gmail.com>
2025-01-25 19:00:48 +0430

    add somthing

--- api/client/src/components/TestimonialsSection.jsx
+++ api/client/src/components/TestimonialsSection.jsx
@@ -6,24 +6,39 @@ function TestimonialsSection() {
   const testimonials = [
     {
       name: "Olivia Walker",
-      image: "https://placekitten.com/80/80", // Placeholder for user image
+      image: "./public/Service-PNG.png", // Placeholder for user image
       text: "This is a very good text reader and tts tool! It generates realistic ai voice. If you aren't sure, always go for Luvvoice. Believe me, you won't regret it.",
     },
     {
       name: "Ashley Taylor",
-      image: "https://placekitten.com/90/90", // Placeholder for user image
+      image: "./public/IMG_8802.JPG", // Placeholder for user image
       text: "Really good. Luvvoice is by far the most valuable business resource we have ever purchased. I love this TTS tool.",
     },
     {
       name: "Emma Rodriguez",
-      image: "https://placekitten.com/100/100", // Placeholder for user image
+      image: "./public/IMG_8802.JPG", // Placeholder for user image
+      text: "The quality of the AI-generated voices is impressive. It's been incredibly helpful for creating accessible content for visually impaired users of my website.",
+    },
+    {
+      name: "Olivia Walker",
+      image: "./public/Service-PNG.png", // Placeholder for user image
+      text: "This is a very good text reader and tts tool! It generates realistic ai voice. If you aren't sure, always go for Luvvoice. Believe me, you won't regret it.",
+    },
+    {
+      name: "Ashley Taylor",
+      image: "./public/IMG_8802.JPG", // Placeholder for user image
+      text: "Really good. Luvvoice is by far the most valuable business resource we have ever purchased. I love this TTS tool.",
+    },
+    {
+      name: "Emma Rodriguez",
+      image: "./public/IMG_8802.JPG", // Placeholder for user image
       text: "The quality of the AI-generated voices is impressive. It's been incredibly helpful for creating accessible content for visually impaired users of my website.",
     },
   ];
 
   return (
     <div className="max-w-6xl mx-auto text-center mb-10">
-      <h2 className="text-3xl font-extrabold text-gray-800 dark:text-white mb-6">
+      <h2 className="text-3xl font-extrabold text-gray-800 dark:text-white mb-6 gap-5 py-6 my-4">
         <span className="text-gray-500 dark:text-white text-sm block uppercase">
           See what our customers have to say about us
         </span>
@@ -37,6 +52,7 @@ function TestimonialsSection() {
             initial={{ opacity: 0, x: 50 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5, delay: index * 0.2 }}
+            
           >
             <div className="flex items-center mb-4">
               <img
